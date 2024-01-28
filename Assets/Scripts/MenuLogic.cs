c67ca77e6d7223e62e3f5a613e394816c244f7f3
Lorne Day <77252924+4Lorne@users.noreply.github.com>
2024-01-28 14:09:29 -0400

    base functionality

--- Assets/Scripts/MenuLogic.cs
+++ Assets/Scripts/MenuLogic.cs
@@ -5,20 +5,13 @@ using UnityEngine;
 public class MenuLogic : MonoBehaviour
 {
     public GameObject mainMenu;
-    public GameObject settingsMenu;
 
     // Start is called before the first frame update
     void Start()
     {
         mainMenu.SetActive(true);
-        settingsMenu.SetActive(false);
     }
-
-    public void OpenSettingsMenu()
-    {
-        mainMenu.SetActive(false);
-        settingsMenu.SetActive(true);
-    }
+    
 
     // Update is called once per frame
     void Update()
